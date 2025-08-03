19febdd0a30c1ea9c37561025ae74c8104fff800
vishnu jeeth <jeeth.pam99@gmail.com>
2025-08-03 19:09:12 +0530

    txt added

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -3,5 +3,5 @@ component=$1
 env=$2
 app_Version=$3
 dnf install ansible -y
-pip3.9 install botocore boto3 -y
-ansible pull -i localhost, -U https://github.com/jeethvishnu/ansiible-infra-roles.git ansible-roles/main.yaml -e component=$component -e environment=$env -e appVersion=$app_Version
+pip3.9 install botocore boto3
+ansible-pull -i localhost, -U https://github.com/jeethvishnu/ansiible-infra-roles.git ansible-roles/main.yaml -e component=$component -e environment=$env -e appVersion=$app_Version
